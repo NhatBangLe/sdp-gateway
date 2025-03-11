bb1ea0a0d306f4fc5805909645f5f8f2e300743e
NhatBangLe <bangle37568@gmail.com>
2025-03-11 13:33:02 +0700

    Add CORS config

--- src/main/java/io/github/nhatbangle/sdp/gateway/config/AppConfig.java
+++ src/main/java/io/github/nhatbangle/sdp/gateway/config/AppConfig.java
@@ -2,14 +2,12 @@ package io.github.nhatbangle.sdp.gateway.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
-@EnableAsync
 @EnableConfigurationProperties(AppConfigurationProperties.class)
 public class AppConfig implements WebMvcConfigurer {
 
